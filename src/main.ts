626cbad57c0ed064f44accd8c047194f053f0dea
siz0001 <42665078+siz0001@users.noreply.github.com>
2022-05-19 16:05:24 +0900

    로그인 모듈 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from "pinia";
 import { router } from './router'
 // import i18n from "./plugins/i18n";
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import './styles/index.css'
 import App from './App.vue'
+import VueAxios from "vue-axios";
+import axios from "./plugins/axios";
+import auth from "./plugins/auth";
 
 loadFonts()
 
-createApp(App).use(vuetify).use(router).mount('#app')
+createApp(App).use(createPinia()).use(auth).use(VueAxios, axios).use(vuetify).use(router).mount('#app')
---
+++ src/store/useAuth.js
@@ -0,0 +1,44 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    loggedIn: localStorage.getItem("token") ? true : false,
+    user: null,
+  }),
+
+  getters: {},
+
+  actions: {
+    async login(credentials) {
+      await axios.get("sanctum/csrf-cookie");
+
+      const response = (await axios.post("api/login", credentials)).data;
+
+      if (response) {
+        const token = `Bearer ${response.token}`;
+
+        localStorage.setItem("token", token);
+        axios.defaults.headers.common["Authorization"] = token;
+
+        await this.ftechUser();
+      }
+    },
+
+    async logout() {
+      const response = (await axios.post("api/logout")).data;
+
+      if (response) {
+        localStorage.removeItem("token");
+
+        this.$reset();
+      }
+    },
+
+    async ftechUser() {
+      this.user = (await axios.get("api/me")).data;
+
+      this.loggedIn = true;
+    },
+  },
+});
